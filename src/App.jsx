92422bbd3b6164a8d0ae7dc619114b73953aebb6
Samuel Raducan <samuel.thembp@gmail.com>
2023-07-28 11:47:56 +0100

    Implement main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 const App = () => {
-  return <div>World Wise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>World Wise</div>;
+};
+
+export default Home;
