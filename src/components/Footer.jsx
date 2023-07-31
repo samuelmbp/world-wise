b209578a774f3fd9725ae7d031a338e4b177b4cf
Samuel Raducan <samuel.thembp@gmail.com>
2023-07-31 09:57:07 +0200

    Refactor: extract footer jsx in a component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import styles from "./Sidebar.module.css";
 
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import Footer from "./Footer";
 
 const Sidebar = () => {
   return (
@@ -10,11 +11,7 @@ const Sidebar = () => {
       <AppNav />
 
       <p>List of cities</p>
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 };
