123f8e8fa15573c9d1ecb24b3ec9c9ccf1e8ed59
Samuel Raducan <samuel.thembp@gmail.com>
2023-08-03 17:24:11 +0200

    Add a back button and a green outline for the selected city

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -3,6 +3,7 @@ import styles from "./City.module.css";
 import { useEffect } from "react";
 import { useCities } from "../context/CitiesContext";
 import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -55,7 +56,9 @@ const City = () => {
         </a>
       </div>
 
-      <div></div>
+      <div>
+        <BackButton />
+      </div>
     </div>
   );
 };
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../context/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,10 +12,16 @@ const formatDate = (date) =>
 const CityItem = ({ city }) => {
   const { cityName, emoji, date, id, position } = city;
   const { lat, lng } = position;
+  const { currentCity } = useCities();
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${lat}&lng=${lng}`}>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${lat}&lng=${lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.cityName}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
